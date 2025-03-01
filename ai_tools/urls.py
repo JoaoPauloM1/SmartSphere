12f587f450edce5f03013bee9de9d4820e392b53
João Paulo Maciel Borges <joaopaulomacielborges@gmail.com>
2025-03-01 13:24:01 -0300

    DRY

--- ai_tools/urls.py
+++ ai_tools/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ai_tools.views import index, chatbot_view
 
 urlpatterns = [
-    path('', index),
-    path('chatbot/', chatbot_view), 
+    path('', index, name='index'),
+    path('chatbot/', chatbot_view, name='chatbot'), 
 ]
