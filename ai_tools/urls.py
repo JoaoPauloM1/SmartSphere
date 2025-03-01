158ff6274442eae6e3517348955f0df84f481b89
João Paulo Maciel Borges <joaopaulomacielborges@gmail.com>
2025-03-01 13:11:31 -0300

    creating chatbot

--- ai_tools/urls.py
+++ ai_tools/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from ai_tools.views import index
+from ai_tools.views import index, chatbot_view
 
 urlpatterns = [
-    path('', index) 
+    path('', index),
+    path('chatbot/', chatbot_view), 
 ]
--- ai_tools/views.py
+++ ai_tools/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from ai_tools.services import chatbot
 
 def index(request):
     return render(request, 'index.html')
+
+def chatbot_view(request):
+    return render(request, 'chatbot.html')
